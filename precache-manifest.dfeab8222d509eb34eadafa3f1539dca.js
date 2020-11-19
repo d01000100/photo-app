d6018816f778ff9e3a7d2133a15c576f965cbfdc
Daniel Casado <jdanielca@gmail.com>
2020-11-19 13:24:16 -0600

    Updates

---
+++ precache-manifest.dfeab8222d509eb34eadafa3f1539dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207a69a3729b876d74af73c8a72dabbd",
+    "url": "/photo-app/index.html"
+  },
+  {
+    "revision": "70aca5a7314845b35dcf",
+    "url": "/photo-app/static/css/main.e68dfed7.chunk.css"
+  },
+  {
+    "revision": "4d891dd8cb2718255e20",
+    "url": "/photo-app/static/js/2.d4fb89a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/photo-app/static/js/2.d4fb89a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70aca5a7314845b35dcf",
+    "url": "/photo-app/static/js/main.7ee97f91.chunk.js"
+  },
+  {
+    "revision": "88b9a3c62925ca29b69a",
+    "url": "/photo-app/static/js/runtime-main.0d57784a.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/photo-app/static/media/logo.8dad8028.svg"
+  }
+]);
